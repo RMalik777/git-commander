445754115229f5decfba271c91afda9e55345199
Rafli Malik <Rafli.Malik@gmail.com>
2024-06-13 22:30:35 +0700

    style: grouping tool item

--- src/components/Tools/Toolbar.jsx
+++ src/components/Tools/Toolbar.jsx
@@ -1,71 +1,92 @@
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "../ui/tooltip";
+} from "@/components/ui/tooltip";
+
 import {
-  GitBranch,
-  GitCompare,
   ArrowDownToLine,
   ArrowUpToLine,
+  GitBranch,
+  GitCompare,
 } from "lucide-react";
 
 export default function Toolbar() {
+  const username = localStorage.getItem("username") || "IDxxxxx";
   return (
-    <div className="border-b border-neutral-200 px-3 py-3">
+    <div className="justify- flex h-fit flex-row items-center justify-between border-b border-neutral-200 px-3 py-3">
       <TooltipProvider delayDuration={50}>
-        <ul className="flex flex-row items-center gap-12">
-          <li>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button size="icon" variant="outline">
-                  <GitBranch />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="bottom">
-                <p>Branch</p>
-              </TooltipContent>
-            </Tooltip>
-          </li>
-          <li>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button size="icon" variant="outline">
-                  <GitCompare />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="bottom">
-                <p>Compare</p>
-              </TooltipContent>
-            </Tooltip>
-          </li>
-          <li>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button size="icon" variant="outline">
-                  <ArrowDownToLine />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="bottom">
-                <p>Pull</p>
-              </TooltipContent>
-            </Tooltip>
-          </li>
-          <li>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button size="icon" variant="outline">
-                  <ArrowUpToLine />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="bottom">
-                <p>Push</p>
-              </TooltipContent>
-            </Tooltip>
-          </li>
-        </ul>
+        <div className="flex flex-row items-center gap-4">
+          <h1>Current Repo</h1>
+          <ul className="flex flex-row items-center gap-12">
+            <li>
+              <div className="flex gap-4">
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button size="icon" variant="outline">
+                      <GitBranch />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent side="bottom">
+                    <p>Branch</p>
+                  </TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button size="icon" variant="outline">
+                      <GitCompare />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent side="bottom">
+                    <p>Compare</p>
+                  </TooltipContent>
+                </Tooltip>
+              </div>
+            </li>
+            <li>
+              <div className="flex gap-4">
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button size="icon" variant="outline">
+                      <ArrowDownToLine />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent side="bottom">
+                    <p>Pull</p>
+                  </TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button size="icon" variant="outline">
+                      <ArrowUpToLine />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent side="bottom">
+                    <p>Push</p>
+                  </TooltipContent>
+                </Tooltip>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="flex h-full w-fit flex-row items-center gap-4">
+          <Separator orientation="vertical" className="h-full" />
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant="outline" size="sm">
+                <p className="text-base">{username}</p>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="bottom">
+              <p>
+                Your <b>username</b> when using git commit
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </div>
       </TooltipProvider>
     </div>
   );
