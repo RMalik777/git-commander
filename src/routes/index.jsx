9cda1fea891c8fd66d11f9ecd78190389fc2323f
Rafli Malik <Rafli.Malik@gmail.com>
2024-06-14 17:36:09 +0700

    feat: add folder viewer

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Command } from "@tauri-apps/api/shell";
 import {
@@ -8,43 +9,132 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { open } from "@tauri-apps/api/dialog";
+import { fs } from "@tauri-apps/api";
 
 export default function Index() {
-  async function trial() {
-    const command = new Command("run-git-log");
+  const [isGitRepo, setIsGitRepo] = useState(true);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [dirList, setDirList] = useState([]);
+
+  async function checkGit(path = repo) {
+    setErrorMsg(null);
+    const command = new Command("git command", ["status", "."], { cwd: path });
     command.on("close", (data) => {
       console.log(
         `command finished with code ${data.code} and signal ${data.signal}`
       );
     });
     command.on("error", (error) => console.error(`command error: "${error}"`));
-    command.stdout.on("data", (line) =>
-      console.log(`command stdout: "${line}"`)
-    );
-    command.stderr.on("data", (line) =>
-      console.log(`command stderr: "${line}"`)
-    );
+    command.stdout.on("data", (data) => {
+      setIsGitRepo(true);
+      console.log(data);
+    });
+    command.stderr.on("data", (line) => {
+      setIsGitRepo(false);
+      console.log(`command stderr: "${line}"`);
+    });
 
-    const child = await command.spawn();
-    console.log("pid:", child.pid);
+    await command.spawn().catch((error) => {
+      setErrorMsg(error);
+      console.error(error);
+    });
   }
+
+  const [repo, setRepo] = useState("path/to/repo");
+  useEffect(() => {
+    async function getAllChildDir(repo) {
+      try {
+        const dir = await fs.readDir(repo, {});
+        console.log(dir);
+        return dir;
+      } catch (error) {
+        console.error(error);
+        return [];
+      }
+    }
+    async function setDirectory() {
+      setDirList(await getAllChildDir(repo));
+    }
+    setRepo(localStorage.getItem("openRepo") || "path/to/repo");
+    console.log("repo", repo);
+    setDirectory();
+  }, [repo]);
+  async function openFile() {
+    const toOpen = await open({
+      multiple: false,
+      directory: true,
+    });
+    if (toOpen) {
+      localStorage.setItem("openRepo", toOpen);
+      setRepo(toOpen);
+    }
+    await checkGit(toOpen);
+  }
+
   return (
-    <div className="min-h-fit">
-      <Card className="">
-        <CardHeader className="pb-3">
-          <CardTitle>Git Repository</CardTitle>
-          <CardDescription className="flex max-w-lg flex-col text-balance leading-relaxed">
-            <code className="w-fit">/path/to/repo</code>
-            <span className="text-red-500">Error! Not a git repository</span>
-          </CardDescription>
-        </CardHeader>
-        <CardFooter>
-          <Button size="sm" variant="default">
-            Change Repository
-          </Button>
-        </CardFooter>
-      </Card>
+    <>
+      <div className="flex min-h-fit flex-row gap-4">
+        <Card className="h-fit flex-grow">
+          <CardHeader className="bg-gray-100 pb-3">
+            <CardTitle>Git Repository</CardTitle>
+            <CardDescription className="flex max-w-full flex-col text-balance leading-relaxed">
+              Select your local git repository
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="pt-3">
+            <p className="w-fit rounded border bg-gray-100 px-2 py-1 text-gray-900">
+              <code className="">{repo}</code>
+            </p>
+            <span className={isGitRepo ? "text-green-600" : "text-red-600"}>
+              {isGitRepo ? "\u00A0" : errorMsg ?? "Error! Not a git repository"}
+            </span>
+          </CardContent>
+          <CardFooter className="flex flex-row gap-2">
+            <Button
+              size="sm"
+              variant="default"
+              onClick={async () => {
+                await openFile();
+              }}>
+              Open Repository
+            </Button>
+            <Button
+              size="sm"
+              variant="secondary"
+              onClick={() => {
+                localStorage.removeItem("openRepo");
+                setRepo("no directory selected");
+              }}>
+              Close Repository
+            </Button>
+            <Button size="sm" variant="outline" onClick={() => {}}>
+              Trial
+            </Button>
+          </CardFooter>
+        </Card>
+
+        <Card className="w-1/3 min-w-[33.33%]">
+          <CardHeader className="bg-gray-100 pb-3">
+            <CardTitle>File List</CardTitle>
+            <CardDescription className="flex max-w-full flex-col text-balance leading-relaxed">
+              Check the files inside the repository
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="pt-3">
+            <ul className="flex flex-col rounded border bg-gray-100 p-2">
+              {dirList?.map((file) => {
+                return (
+                  <li key={file.path} className="w-fit">
+                    <code className="w-fit text-sm">{file.name}</code>
+                  </li>
+                );
+              })}
+            </ul>
+          </CardContent>
+        </Card>
+      </div>
       <h1>Index</h1>
-    </div>
+    </>
   );
 }
