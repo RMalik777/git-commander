8386de4c18885b8a3009bf9c7fd21a4bfec4bd19
Rafli Malik <Rafli.Malik@gmail.com>
2024-06-14 14:41:45 +0700

    fix: error now showing clearer information

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -10,9 +10,18 @@ export default function ErrorPage() {
       className="min-w-dvw mx-12 flex min-h-full flex-col items-center justify-center gap-2">
       <h1 className="text-4xl">oops! :(</h1>
       <p className="text-2xl">Sorry, an unexpected error has occurred.</p>
-      <p className="">
-        <code className="">{error.statusText || error.message}</code>
+      <p className="text-lg">
+        <code className="">
+          <span className="font-bold">
+            {error.status ? error.status + " | " : ""}
+          </span>
+          {error.statusText || error.message}
+        </code>
       </p>
+      {/* <p className="self-start text-sm"> Complete Error:</p>
+      <code className="whitespace-pre-wrap rounded border bg-gray-200 p-2 font-mono text-sm">
+        {error?.error?.stack ? error.error.stack : error.stack}
+      </code> */}
     </div>
   );
 }
