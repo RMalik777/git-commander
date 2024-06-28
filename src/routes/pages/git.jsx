f1e737727d17a71ecca69008539cf932afa3b9e7
Rafli Malik <Rafli.Malik@gmail.com>
2024-06-28 17:01:55 +0700

    chore: remove unused import

--- src/routes/pages/git.jsx
+++ src/routes/pages/git.jsx
@@ -1,19 +1,3 @@
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-
-import { CloudDownload } from "lucide-react";
-
 import Clone from "@/components/Dialog/Clone";
 
 export default function Git() {
