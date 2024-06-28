4ee5cfedc235fac8b6d1fb13f03976de57ce3855
Rafli Malik <Rafli.Malik@gmail.com>
2024-06-28 17:01:51 +0700

    fix: fix tailwind not applying in jsx file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx,jsx,js}',
+    './components/**/*.{ts,tsx,jsx,js}',
+    './app/**/*.{ts,tsx,js,jsx}',
+    './src/**/*.{ts,tsx,js,jsx}',
   ],
   prefix: "",
   theme: {
