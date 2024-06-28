d75956d4f85e212e769d99d14ca64f0338c7fb20
Rafli Malik <Rafli.Malik@gmail.com>
2024-06-28 17:04:02 +0700

    fix: change context with redux

---
+++ src/lib/Redux/userSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const userSlice = createSlice({
+  name:"user",
+  initialState:{
+    value: localStorage.getItem("username") || ""
+  },
+  reducers:{
+    setUser: (state, action) => {
+      state.value = action.payload
+    },
+    removeUser: (state) => {
+      state.value = ""
+    }
+  }
+})
+export const { setUser, removeUser } = userSlice.actions
+export default userSlice.reducer
