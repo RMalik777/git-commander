dd46338d00151f5b260e529b22c8749ec9b48b02
Rafli Malik <Rafli.Malik@gmail.com>
2024-06-28 17:04:33 +0700

    fix: add types for repo item and update dialog

--- src/components/Dialog/AddRepo.tsx
+++ src/components/Dialog/AddRepo.tsx
@@ -1,6 +1,12 @@
 import { Button } from "@/components/ui/button";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -8,10 +14,50 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { useToast } from "../ui/use-toast";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: "Please enter a name" })
+    .max(255, { message: "Name is too long" }),
+  link: z.string().url({ message: "Please enter a valid URL" }),
+});
 
 export default function AddRepo() {
+  const addRepoForm = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      link: "",
+    },
+  });
+  const { handleSubmit, reset } = addRepoForm;
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    reset();
+    toast({
+      title: "Repository added",
+      description: `Repository ${values.name} added successfully`,
+    });
+  }
+  const { toast } = useToast();
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -24,29 +70,66 @@ export default function AddRepo() {
             Make changes to your profile here. Click save when you&apos;re done.
           </DialogDescription>
         </DialogHeader>
-        <form className="flex flex-col gap-4">
-          <div>
-            <Label htmlFor="name">Repositories Name</Label>
-            <Input type="text" id="name" placeholder="Name" className="" />
-          </div>
-          <div>
-            <Label htmlFor="link">Link</Label>
-            <Input
-              type="text"
-              id="link"
-              placeholder="example.com"
-              className=""
+        <Form {...addRepoForm}>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit(onSubmit)}>
+            <FormField
+              control={addRepoForm.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Repository Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Repository Name"
+                      type="text"
+                    />
+                  </FormControl>
+                  <div>
+                    <FormDescription>
+                      Repository name that will be shown
+                    </FormDescription>
+                    <FormMessage /> 
+                  </div>
+                </FormItem>
+              )}
             />
-          </div>
-          <DialogFooter>
-            <Button variant="outline" type="reset">
-              Cancel
-            </Button>
-            <Button variant="default" type="submit">
-              Add
-            </Button>
-          </DialogFooter>
-        </form>
+            <FormField
+              control={addRepoForm.control}
+              name="link"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Repository Link</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Repository Link"
+                      type="text"
+                    />
+                  </FormControl>
+                  <div>
+                    <FormDescription>
+                      Link to corresponding remote repository
+                    </FormDescription>
+                    <FormMessage />
+                  </div>
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="secondary" onClick={() => reset()}>
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button variant="default" type="submit">
+                Add
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
---
+++ src/lib/Types/repo.ts
@@ -0,0 +1,6 @@
+export interface RepoFormat {
+  id: string;
+  repo_name: string;
+  repo_url: string;
+  created_at: Date;
+}
