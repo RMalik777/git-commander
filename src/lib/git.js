a9caaa2b784add54780ac2d0025e2b8fb17af6ea
Rafli Malik <Rafli.Malik@gmail.com>
2024-06-28 17:04:18 +0700

    refactor: add more git function and refactor to a clearer function

--- src/lib/git.js
+++ src/lib/git.js
@@ -12,15 +12,11 @@ export async function checkGit(path) {
       resolve({ isGitRepo, errorMsg });
     });
     command.on("error", (error) => {
-      console.error(`command error: "${error}"`);
+      console.error(error);
       reject(new Error(error));
     });
-    command.stdout.on("data", (data) => {
-      isGitRepo = true;
-    });
-    command.stderr.on("data", (line) => {
-      isGitRepo = false;
-    });
+    command.stdout.on("data", () => (isGitRepo = true));
+    command.stderr.on("data", () => (isGitRepo = false));
     command.spawn().catch((error) => {
       errorMsg = error;
       console.error(error);
@@ -50,81 +46,80 @@ export async function setSSLFalse() {
 }
 
 export async function push(path) {
-  const command = new Command("git 1 args", ["push"], { cwd: path });
-  command.on("close", (data) => {
-    console.log(
-      `command finished with code ${data.code} and signal ${data.signal}`
-    );
+  const response = new Promise((resolve, reject) => {
+    const result = [];
+    const command = new Command("git 1 args", ["push"], { cwd: path });
+    command.on("close", () => resolve(result));
+    command.on("error", (error) => reject(new Error(error)));
+    command.stdout.on("data", (line) => result.push(line));
+    command.stderr.on("data", (line) => result.push(line));
+    command.spawn().catch((error) => reject(new Error(error)));
   });
-  command.on("error", (error) => console.error(`command error: "${error}"`));
-  command.stdout.on("data", (line) => {
-    console.log(`command stdout: "${line}"`);
-  });
-  command.stderr.on("data", (line) => {
-    console.log(`command stderr: "${line}"`);
-  });
-  await command.spawn().catch((error) => {
-    console.error(error);
-  });
+  return await response;
 }
 export async function pull(path) {
-  const command = new Command("git 1 args", ["pull"], { cwd: path });
-  command.on("close", (data) => {
-    console.log(
-      `command finished with code ${data.code} and signal ${data.signal}`
-    );
+  const response = new Promise((resolve, reject) => {
+    const result = [];
+    const command = new Command("git 1 args", ["pull"], { cwd: path });
+    command.on("close", () => resolve(result));
+    command.on("error", (error) => reject(new Error(error)));
+    command.stdout.on("data", (line) => result.push(line));
+    command.stderr.on("data", (line) => result.push(line));
+    command.spawn().catch((error) => reject(new Error(error)));
   });
-  command.on("error", (error) => console.error(`command error: "${error}"`));
-  command.stdout.on("data", (line) => {
-    console.log(`command stdout: "${line}"`);
-  });
-  command.stderr.on("data", (line) => {
-    console.log(`command stderr: "${line}"`);
-  });
-  await command.spawn().catch((error) => {
-    console.error(error);
-  });
+  return await response;
 }
 
 export async function clone(localRepo, remoteRepo) {
-  const command = new Command("git 2 args", ["clone", remoteRepo], {
-    cwd: localRepo,
+  const response = new Promise((resolve, reject) => {
+    const result = [];
+    const command = new Command("git 2 args", ["clone", remoteRepo], {
+      cwd: localRepo,
+    });
+    command.on("close", (data) => {
+      console.log(
+        `command finished with code ${data.code} and signal ${data.signal}`
+      );
+      resolve(result);
+    });
+    command.on("error", (error) => {
+      console.error(`command error: "${error}"`);
+      reject(new Error(error));
+    });
+    command.stdout.on("data", (line) => result.push(line));
+    command.stderr.on("data", (line) => result.push(line));
+    command.spawn().catch((error) => {
+      console.error(error);
+      reject(new Error(error));
+    });
   });
-  command.on("close", (data) => {
-    console.log(
-      `command finished with code ${data.code} and signal ${data.signal}`
-    );
-  });
-  command.on("error", (error) => console.error(`command error: "${error}"`));
-  command.stdout.on("data", (line) => {
-    console.log(`command stdout: "${line}"`);
-  });
-  command.stderr.on("data", (line) => {
-    console.log(`command stderr: "${line}"`);
-  });
-  await command.spawn().catch((error) => {
-    console.error(error);
-  });
+  return await response;
 }
 
 export async function status(path) {
-  const command = new Command("git 1 args", ["status"], { cwd: path });
-  command.on("error", (error) => console.error(`command error: "${error}"`));
-  command.stdout.on("data", (data) => {
-    console.log(data);
+  const response = new Promise((resolve, reject) => {
+    const result = [];
+    const command = new Command("git 1 args", ["status"], { cwd: path });
+    command.on("error", (error) => {
+      console.error(`command error: "${error}"`);
+      reject(new Error(error));
+    });
+    command.stdout.on("data", (data) => console.log(data));
+    command.stderr.on("data", (line) =>
+      result.push(line.replace(/[\n\r]/g, ""))
+    );
+    command.spawn().catch((error) => {
+      console.error(error);
+      reject(new Error(error));
+    });
   });
-  command.stderr.on("data", (line) => {
-    console.log(`command stderr: "${line}"`);
-  });
-  await command.spawn().catch((error) => {
-    console.error(error);
-  });
+  return await response;
 }
 
 export async function currentBranch(path) {
-  const regex = /(?:on branch) ([\S\s]+)/gi;
-  const command = new Command("git 1 args", ["status"], { cwd: path });
   const response = new Promise((resolve, reject) => {
+    const regex = /(?:on branch) ([\S\s]+)/gi;
+    const command = new Command("git 1 args", ["status"], { cwd: path });
     let result;
     command.on("error", (error) => {
       console.error(`command error: "${error}"`);
@@ -136,23 +131,20 @@ export async function currentBranch(path) {
         resolve(result.trim());
       }
     });
-    command.stderr.on("data", (line) => {
-      console.log(`command stderr: "${line}"`);
-    });
+    command.stderr.on("data", (line) =>
+      console.log(`command stderr: "${line}"`)
+    );
     command.spawn().catch((error) => {
       console.error(error);
       reject(new Error(error));
     });
   });
-  const result = await response.catch((error) => {
-    return error(error);
-  });
-  return result;
+  return await response;
 }
 
 export async function branchList(path) {
-  const command = new Command("git 2 args", ["branch", "-r"], { cwd: path });
-  const result = new Promise((resolve, reject) => {
+  const response = new Promise((resolve, reject) => {
+    const command = new Command("git 2 args", ["branch", "-r"], { cwd: path });
     const result = [];
     command.on("error", (error) => console.error(`command error: "${error}"`));
     command.stdout.on("data", (data) => {
@@ -166,17 +158,10 @@ export async function branchList(path) {
       console.log(`command stderr: "${line}"`);
       reject(new Error(line));
     });
-    command.on("close", () => {
-      resolve(result);
-    });
-    command.spawn().catch((error) => {
-      console.error(error);
-    });
+    command.on("close", () => resolve(result));
+    command.spawn().catch((error) => console.error(error));
   });
-  const response = await result.catch((error) => {
-    console.error(error);
-  });
-  return response;
+  return await response;
 }
 
 export async function switchBranch(path, branch) {
@@ -197,9 +182,9 @@ export async function showChanged(path) {
     command.stdout.on("data", (line) => {
       result.push(line.trim().replace(/[\n\r]/g, ""));
     });
-    command.stderr.on("data", (line) => {
-      console.log(`command stderr: "${line}"`);
-    });
+    command.stderr.on("data", (line) =>
+      console.log(`command stderr: "${line}"`)
+    );
     command.spawn().catch((error) => {
       reject(new Error(error));
     });
@@ -232,24 +217,76 @@ export async function showStaged(path) {
   return await response;
 }
 
-export async function commitAll(path, message) {
+export async function addAll(path) {
+  const command = new Command("git 2 args", ["add", "."], { cwd: path });
+  await command.spawn().catch((error) => {
+    console.error(error);
+  });
+}
+export async function addFile(path, file) {
+  const command = new Command("git 3 args", ["add", file], { cwd: path });
+  await command.spawn().catch((error) => {
+    console.error(error);
+  });
+}
+
+export async function commit(path, message) {
   const response = new Promise((resolve, reject) => {
-    const command = new Command("git 3 args", ["commit", "-m", message], {
+    const commmit = new Command("git 3 args", ["commit", "-m", message], {
       cwd: path,
     });
-    command.on("close", (data) => {
+    commmit.on("close", (data) => {
       console.log(
         `command finished with code ${data.code} and signal ${data.signal}`
       );
       resolve(data);
     });
-    command.on("error", (error) => console.error(`command error: "${error}"`));
-    command.stdout.on("data", (line) => {
-      console.log(`command stdout: "${line}"`);
+    commmit.on("error", (error) => console.error(`command error: "${error}"`));
+    commmit.stdout.on("data", (line) =>
+      console.log(`command stdout: "${line}"`)
+    );
+    commmit.stderr.on("data", (line) =>
+      console.log(`command stderr: "${line}"`)
+    );
+    commmit.spawn().catch((error) => {
+      reject(new Error(error));
     });
-    command.stderr.on("data", (line) => {
-      console.log(`command stderr: "${line}"`);
+  });
+  return await response;
+}
+
+export async function commitAll(path, message) {
+  await addAll(path);
+  return await commit(path, message);
+}
+
+export async function undoLastCommit(path) {
+  const response = new Promise((resolve, reject) => {
+    const result = [];
+    const command = new Command("git 2 args", ["reset", "--soft"], {
+      cwd: path,
     });
+    command.on("close", () => resolve(result));
+    command.on("error", (error) => reject(new Error(error)));
+    command.stdout.on("data", (line) => result.push(line));
+    command.stderr.on("data", (line) => result.push(line));
+    command.spawn().catch((error) => {
+      reject(new Error(error));
+    });
+  });
+  return await response;
+}
+
+export async function undoStaged(path) {
+  const response = new Promise((resolve, reject) => {
+    const result = [];
+    const command = new Command("git 1 args", ["reset"], {
+      cwd: path,
+    });
+    command.on("close", () => resolve(result));
+    command.on("error", (error) => reject(new Error(error)));
+    command.stdout.on("data", (line) => result.push(line));
+    command.stderr.on("data", (line) => result.push(line));
     command.spawn().catch((error) => {
       reject(new Error(error));
     });
