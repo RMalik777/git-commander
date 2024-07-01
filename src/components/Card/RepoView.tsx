c7803fc131167614d13cd8d4feb7eb45e5faecd7
Rafli Malik <Rafli.Malik@gmail.com>
2024-07-01 09:09:05 +0700

    refactor: extract card to component

---
+++ src/components/Card/RepoView.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+import { open } from "@tauri-apps/api/dialog";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import * as git from "@/lib/git";
+
+import Clone from "@/components/Dialog/Clone";
+
+import { useAppSelector, useAppDispatch } from "@/lib/Redux/hooks";
+import { removeRepo, setRepo } from "@/lib/Redux/repoSlice";
+
+export default function RepoView() {
+  const dir = useAppSelector((state) => state.repo.directory);
+  const dispatch = useAppDispatch();
+
+  const [isGitRepo, setIsGitRepo] = useState(true);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    if (!dir || dir == "") return;
+    async function getDiff() {
+      const data = await git.showChanged(dir);
+      dispatch(setRepo({ diff: data }));
+    }
+    getDiff();
+  }, [dir]);
+
+  useEffect(() => {
+    if (!dir || dir == "") return;
+    async function getStaged() {
+      const data = await git.showStaged(dir);
+      dispatch(setRepo({ staged: data }));
+    }
+    getStaged();
+  }, [dir]);
+
+  async function openFile() {
+    const toOpen = await open({
+      multiple: false,
+      directory: true,
+    });
+    if (typeof toOpen === "string") {
+      localStorage.setItem("repoDir", toOpen.toString());
+      const repoNameNew = toOpen.split("\\").pop();
+      if (repoNameNew)
+        localStorage.setItem("currentRepoName", repoNameNew.toString());
+      dispatch(setRepo({ name: repoNameNew, directory: toOpen }));
+    }
+    const data = await git.checkGit(toOpen);
+    setIsGitRepo(data.isGitRepo);
+    setErrorMsg(data.errorMsg);
+  }
+
+  return (
+    <Card className="h-fit flex-grow">
+      <CardHeader className="bg-gray-100 pb-3">
+        <CardTitle>Git Repository</CardTitle>
+        <CardDescription className="flex max-w-full flex-col text-balance leading-relaxed">
+          Select your local git repository
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="pt-3">
+        <p className="w-fit rounded border bg-gray-100 px-2 py-1 text-gray-900">
+          <code className="">{dir}</code>
+        </p>
+        <span className={isGitRepo ? "text-green-600" : "text-red-600"}>
+          {isGitRepo ? "\u00A0" : errorMsg ?? "Error! Not a git repository"}
+        </span>
+      </CardContent>
+      <CardFooter className="flex flex-row gap-2">
+        <Button
+          size="sm"
+          variant="default"
+          onClick={async () => {
+            await openFile();
+          }}>
+          Open Repository
+        </Button>
+        <Clone />
+        <Button
+          size="sm"
+          variant="destructive"
+          onClick={() => {
+            localStorage.removeItem("repoDir");
+            localStorage.removeItem("currentRepoName");
+            dispatch(removeRepo());
+          }}>
+          Close Repository
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,23 +1,9 @@
-import { useEffect, useLayoutEffect, useState } from "react";
-import { open } from "@tauri-apps/api/dialog";
-
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
-import * as git from "@/lib/git";
+import { useEffect } from "react";
 
 import Commit from "@/components/Git/Commit";
-import Clone from "@/components/Dialog/Clone";
+import RepoView from "@/components/Card/RepoView";
 
-import { useSelector, useDispatch } from "react-redux";
-import { removeRepo, setRepo } from "@/lib/Redux/repoSlice";
+import * as git from "@/lib/git";
 
 export default function Index() {
   useEffect(() => {
@@ -26,86 +12,10 @@ export default function Index() {
     }
     setSLL();
   }, []);
-  
-  const dir = useSelector((state) => state.repo.directory);
-  const dispatch = useDispatch();
-
-  const [isGitRepo, setIsGitRepo] = useState(true);
-  const [errorMsg, setErrorMsg] = useState(null);
-  useEffect(() => {
-    if (!dir || dir == "") return;
-    async function getDiff() {
-      const data = await git.showChanged(dir);
-      dispatch(setRepo({ diff: data }));
-    }
-    getDiff();
-  }, [dir]);
-
-  useEffect(() => {
-    if (!dir || dir == "") return;
-    async function getStaged() {
-      const data = await git.showStaged(dir);
-      dispatch(setRepo({ staged: data }));
-    }
-    getStaged();
-  }, [dir]);
-
-  async function openFile() {
-    const toOpen = await open({
-      multiple: false,
-      directory: true,
-    });
-    if (toOpen) {
-      localStorage.setItem("repoDir", toOpen);
-      const repoNameNew = toOpen.split("\\").pop();
-      localStorage.setItem("currentRepoName", repoNameNew);
-      dispatch(setRepo({ name: repoNameNew, directory: toOpen }));
-    }
-    const data = await git.checkGit(toOpen);
-    setIsGitRepo(data.isGitRepo);
-    setErrorMsg(data.errorMsg);
-  }
-
   return (
     <>
       <div className="flex min-h-fit flex-col gap-4 lg:flex-row">
-        <Card className="h-fit flex-grow">
-          <CardHeader className="bg-gray-100 pb-3">
-            <CardTitle>Git Repository</CardTitle>
-            <CardDescription className="flex max-w-full flex-col text-balance leading-relaxed">
-              Select your local git repository
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="pt-3">
-            <p className="w-fit rounded border bg-gray-100 px-2 py-1 text-gray-900">
-              <code className="">{dir}</code>
-            </p>
-            <span className={isGitRepo ? "text-green-600" : "text-red-600"}>
-              {isGitRepo ? "\u00A0" : errorMsg ?? "Error! Not a git repository"}
-            </span>
-          </CardContent>
-          <CardFooter className="flex flex-row gap-2">
-            <Button
-              size="sm"
-              variant="default"
-              onClick={async () => {
-                await openFile();
-              }}>
-              Open Repository
-            </Button>
-            <Clone />
-            <Button
-              size="sm"
-              variant="destructive"
-              onClick={() => {
-                localStorage.removeItem("repoDir");
-                localStorage.removeItem("currentRepoName");
-                dispatch(removeRepo());
-              }}>
-              Close Repository
-            </Button>
-          </CardFooter>
-        </Card>
+        <RepoView />
         <Commit />
       </div>
       <h1>Index</h1>
