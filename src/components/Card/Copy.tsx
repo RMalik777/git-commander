208c56adaa7b71d2fd881a10ba68a5cd2b9a0067
Rafli Malik <Rafli.Malik@gmail.com>
2024-08-28 14:43:23 +0700

    fix: change input to multiple files

--- src/components/Card/Copy.tsx
+++ src/components/Card/Copy.tsx
@@ -29,12 +29,15 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { ToastAction } from "@/components/ui/toast";
 import { useToast } from "@/components/ui/use-toast";
 
-import { MoveRight } from "lucide-react";
+import { MoveRight, TriangleAlert } from "lucide-react";
 
 const formSchema = z.object({
+  fileList: z.string().min(1, { message: "Please enter the file name" }),
+  overwrite: z.boolean().default(false).optional(),
   source: z.string().min(1, { message: "Please choose source" }),
   destination: z.string().min(1, { message: "Please choose destination" }),
   rememberSource: z.boolean().default(false).optional(),
@@ -52,6 +55,8 @@ export function Copy() {
   const copyForm = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      fileList: "",
+      overwrite: false,
       source: source,
       destination: destination,
       rememberSource: false,
@@ -61,24 +66,88 @@ export function Copy() {
   const { handleSubmit, reset } = copyForm;
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    const targetFileName = values.source.split("\\").pop();
-    try {
-      if (await exists(values.destination + "\\" + targetFileName)) {
-        throw new Error(
-          "File with same name already exists in destination folder."
-        );
+    const list = values.fileList.trim().split("\n");
+    const listLength = list.length;
+    const duplicateList = [];
+    for (let i = listLength - 1; i >= 0; i--) {
+      const file = list[i].trim();
+      if (file.length === 0) {
+        list.splice(i, 1);
+      } else {
+        list[i] = file;
+        if (!(await exists(`${values.source}/${file}`))) {
+          toast({
+            title: "Copying Cancelled",
+            description: (
+              <>
+                File <code className="rounded bg-red-700 p-1">{file}</code> does
+                not exist in the source folder. Please check the file name and
+                location
+              </>
+            ),
+            variant: "destructive",
+            action: (
+              <ToastAction
+                altText="Open Folder"
+                onClick={async () => {
+                  await openFolder(values.source);
+                }}>
+                Open Folder
+              </ToastAction>
+            ),
+          });
+          return;
+        }
+        if (
+          (await exists(`${values.destination}/${file}`)) &&
+          !values.overwrite
+        ) {
+          duplicateList.push(file);
+        }
       }
-      if (!(await exists(values.source))) {
-        throw new Error(
-          "Source file does not exist. Make sure the file is not moved or deleted."
-        );
-      }
+    }
+    if (list.length === 0) {
+      toast({
+        title: "Copying Cancelled",
+        description: "No file to copy",
+        variant: "destructive",
+      });
+      return;
+    }
+    if (duplicateList.length > 0) {
+      toast({
+        title: "Copying Cancelled",
+        description: (
+          <>
+            File{" "}
+            <code className="rounded bg-black/10 p-1 font-medium text-white">
+              {duplicateList.join(", ")}
+            </code>{" "}
+            already exists in the destination folder.
+          </>
+        ),
+        variant: "destructive",
+        action: (
+          <ToastAction
+            altText="Open Folder"
+            onClick={async () => {
+              await openFolder(values.destination);
+            }}>
+            Open Folder
+          </ToastAction>
+        ),
+      });
+      return;
+    }
+    list.forEach(async (item) => {
       try {
         await copyFile(
-          values.source,
-          values.destination + "\\" + targetFileName
+          `${values.source}/${item}`,
+          `${values.destination}/${item}`,
+          {
+            append: false,
+          }
         );
-        reset();
         toast({
           title: "File Copied",
           description: "The file has been copied successfully",
@@ -92,30 +161,25 @@ export function Copy() {
             </ToastAction>
           ),
         });
-        if (values.rememberSource) {
-          localStorage.setItem("source", values.source);
-        }
-        if (values.rememberDestination) {
-          localStorage.setItem("destination", values.destination);
-        }
+        reset();
       } catch (error) {
-        throw Error(error as string);
+        console.error(error);
+        if (error instanceof Error) {
+          toast({
+            title: "Copying Cancelled",
+            description: error.message,
+            variant: "destructive",
+          });
+        } else {
+          toast({
+            title: "Copying Cancelled",
+            description: "An unknown error occurred while copying the file.",
+            variant: "destructive",
+          });
+        }
       }
-    } catch (error) {
-      if (error instanceof Error) {
-        toast({
-          title: "Error Copying File",
-          description: error.message,
-          variant: "destructive",
-        });
-      } else {
-        toast({
-          title: "Error Copying File",
-          description: "An unknown error occured while copying the file",
-          variant: "destructive",
-        });
-      }
-    }
+    });
+    console.log(list);
   }
   return (
     <Card>
@@ -128,19 +192,60 @@ export function Copy() {
           <form
             onSubmit={handleSubmit(onSubmit)}
             className="flex flex-col items-end gap-4">
+            <FormField
+              control={copyForm.control}
+              name="fileList"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>File Name</FormLabel>
+                  <FormControl>
+                    <div className="space-y-2">
+                      <Textarea
+                        className=""
+                        {...field}
+                        placeholder={`example.txt\nexample2.txt\nexample3.txt`}
+                      />
+                      <FormDescription>
+                        Enter the file name you want to copy, separated by line/
+                        {/* */}
+                        <span className="rounded bg-neutral-200 px-2 py-1 font-mono text-neutral-900 dark:bg-neutral-800 dark:text-neutral-50">
+                          Enter
+                        </span>
+                      </FormDescription>
+                      <FormMessage />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}></FormField>
+            <FormField
+              control={copyForm.control}
+              name="overwrite"
+              render={({ field }) => (
+                <FormItem className="relative flex w-full flex-row items-start gap-2 space-y-0 self-start">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <FormLabel>
+                    Overwrite Files with Same Name in Destination
+                  </FormLabel>
+                </FormItem>
+              )}></FormField>
             <div className="flex w-full flex-col items-start justify-between gap-4 md:flex-row">
               <FormField
                 control={copyForm.control}
                 name="source"
                 render={({ field }) => (
                   <FormItem className="w-full">
-                    <FormLabel>Source</FormLabel>
+                    <FormLabel>Source Folder</FormLabel>
                     <FormControl>
                       <div className="flex flex-row gap-2">
                         <div className="w-full">
                           <Input {...field} />
                           <FormDescription>
-                            Choose the file you want to copy
+                            Choose the folder where the source file is located.
                           </FormDescription>
                           <FormMessage />
                         </div>
@@ -151,6 +256,7 @@ export function Copy() {
                           onClick={async () => {
                             const toOpen = await open({
                               multiple: true,
+                              directory: true,
                               defaultPath: currentRepoDir,
                             });
                             if (toOpen) {
@@ -171,13 +277,14 @@ export function Copy() {
                 name="destination"
                 render={({ field }) => (
                   <FormItem className="w-full">
-                    <FormLabel>Destination</FormLabel>
+                    <FormLabel>Destination Folder</FormLabel>
                     <FormControl>
                       <div className="flex flex-row gap-2">
                         <div className="w-full">
                           <Input {...field} />
                           <FormDescription>
-                            Choose the destination folder
+                            Choose the destination folder where the file will
+                            be.
                           </FormDescription>
                           <FormMessage />
                         </div>
@@ -246,7 +353,22 @@ export function Copy() {
                 )}></FormField>
             </div>
 
-            <div className="space-x-2">
+            <div className="flex w-full flex-col items-center justify-end gap-2 md:flex-row md:gap-4">
+              <div
+                className={
+                  (copyForm.getValues("overwrite") ?
+                    "scale-100 opacity-100 max-md:translate-y-0 md:translate-x-0"
+                  : "scale-0 opacity-0 max-md:translate-y-6 md:translate-x-40") +
+                  " flex w-fit items-center gap-1 rounded border border-yellow-500/50 bg-yellow-200/20 p-2 text-yellow-700 duration-200 ease-out dark:border-yellow-500 dark:border-yellow-900/50 dark:dark:border-yellow-900 dark:bg-yellow-800/20 dark:text-yellow-500 [&>svg]:text-yellow-700 dark:[&>svg]:text-yellow-500"
+                }>
+                <TriangleAlert size={16} />
+                <p className="text-center font-medium">
+                  Warning!{" "}
+                  <span className="font-normal">
+                    Overwrite option is turned on
+                  </span>
+                </p>
+              </div>
               <Button type="submit">Copy</Button>
             </div>
           </form>
