dc570c1d2105ad2e791292fc0789b72f851b3b9f
Rafli Malik <Rafli.Malik@gmail.com>
2024-08-29 15:06:55 +0700

    fix: file list refresh every page changes

--- src/lib/directory.ts
+++ src/lib/directory.ts
@@ -1,10 +1,14 @@
 import { readDir } from "@tauri-apps/api/fs";
 import { sortAndFilter } from "./functions";
+import { Store } from "tauri-plugin-store-api";
 
 export async function getAllChildDir(repo: string) {
+  const fileStore = new Store(".fileList.json");
   try {
     const directory = await readDir(repo, { recursive: true });
     const sorted = await sortAndFilter(directory, repo);
+    await fileStore.set("fileList", sorted);
+    await fileStore.save();
     return sorted;
   } catch (error) {
     console.error(error);
