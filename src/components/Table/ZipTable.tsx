0498923cb076764c739fe05c21f30ab599470cb8
Rafli Malik <Rafli.Malik@gmail.com>
2024-10-04 14:42:25 +0700

    fix: item in zip table is refusing to swap position

--- src/components/Table/ZipTable.tsx
+++ src/components/Table/ZipTable.tsx
@@ -59,135 +59,143 @@ export function ZipTable({
       // ref={parent}
       >
         {filteredFileList?.length > 0 ?
-          filteredFileList?.map((item, index) => (
-            <TableRow key={item.path}>
-              <TableCell className="w-fit min-w-16 py-0 text-center">
-                <>
-                  {isEdit && editId == index ?
-                    <form
-                      ref={form}
-                      className="h-full w-min min-w-full"
-                      onSubmit={(e) => {
-                        e.preventDefault();
-                        if (editValue != index + 1) {
-                          let newFiltered;
-                          // If the new place is higher in the table
-                          if (editValue < index + 1) {
-                            newFiltered = filteredFileList.toSpliced(editValue - 1, 0, item);
-                            newFiltered.splice(index + 1, 1);
-                          } else {
-                            newFiltered = filteredFileList.toSpliced(editValue, 0, item);
-                            newFiltered.splice(index, 1);
+          filteredFileList?.map((item, index) => {
+            const realIndex = fileList.findIndex((find) => find.path === item.path);
+            return (
+              <TableRow key={item.path}>
+                <TableCell className="w-fit min-w-16 py-0 text-center">
+                  <>
+                    {isEdit && editId == realIndex ?
+                      <form
+                        ref={form}
+                        className="h-full w-min min-w-full"
+                        onSubmit={(e) => {
+                          e.preventDefault();
+                          if (editValue != realIndex + 1) {
+                            // If the new place is higher in the table
+                            if (editValue < realIndex + 1) {
+                              filteredFileList.splice(editValue - 1, 0, item);
+                              filteredFileList.splice(index + 1, 1);
+                              fileList.splice(editValue - 1, 0, item);
+                              fileList.splice(realIndex + 1, 1);
+                            } else {
+                              filteredFileList.splice(editValue, 0, item);
+                              filteredFileList.splice(index, 1);
+                              fileList.splice(editValue, 0, item);
+                              fileList.splice(realIndex, 1);
+                            }
+                            setFileList([...fileList]);
                           }
-                        }
-                        setIsEdit(false);
-                      }}>
-                      <Input
-                        className="h-full w-full p-0 px-0 py-1 text-center font-medium"
-                        type="number"
-                        min={1}
-                        max={filteredFileList.length}
-                        value={editValue}
-                        onChange={(e) => setEditValue(parseInt(e.target.value))}
-                        onBlur={() => form.current?.requestSubmit()}
-                        autoFocus
+                          setIsEdit(false);
+                        }}>
+                        <Input
+                          className="h-full w-full p-0 px-0 py-1 text-center font-medium"
+                          type="number"
+                          min={1}
+                          max={fileList.length}
+                          value={editValue}
+                          onChange={(e) => setEditValue(parseInt(e.target.value))}
+                          onBlur={() => form.current?.requestSubmit()}
+                          autoFocus
+                          required
+                        />
+                      </form>
+                    : <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => {
+                          setEditValue(realIndex + 1);
+                          setEditId(realIndex);
+                          setIsEdit(true);
+                        }}>
+                        {realIndex + 1}
+                      </Button>
+                    }
+                  </>
+                </TableCell>
+                <TableCell className="">
+                  <button
+                    className="flex items-center gap-1 text-left font-semibold"
+                    onClick={() => {
+                      open(item.path);
+                    }}>
+                    {item.type == "Folder" ?
+                      <Folder
+                        size={16}
+                        className="fill-yellow-400 text-yellow-400 dark:fill-yellow-500 dark:text-yellow-500"
                       />
-                    </form>
-                  : <Button
+                    : Icons({ name: item.name })}
+                    {item.name}
+                  </button>
+                </TableCell>
+                <TableCell className="break-all">{item.path}</TableCell>
+                <TableCell>
+                  <div className="flex flex-col items-center justify-center gap-1 sm:flex-row">
+                    <Button
+                      size="icon"
+                      className="h-8 w-8"
                       variant="outline"
-                      size="sm"
+                      disabled={index === 0}
                       onClick={() => {
-                        setEditValue(index + 1);
-                        setEditId(index);
-                        setIsEdit(true);
+                        [filteredFileList[index], filteredFileList[index - 1]] = [
+                          filteredFileList[index - 1],
+                          filteredFileList[index],
+                        ];
+                        const toSwap1 = fileList.findIndex(
+                          (find) => find.path === filteredFileList[index].path
+                        );
+                        const toSwap2 = fileList.findIndex(
+                          (find) => find.path === filteredFileList[index - 1].path
+                        );
+                        [fileList[toSwap1], fileList[toSwap2]] = [
+                          fileList[toSwap2],
+                          fileList[toSwap1],
+                        ];
+                        setFileList([...fileList]);
                       }}>
-                      {index + 1}
+                      <ChevronUp />
                     </Button>
-                  }
-                </>
-              </TableCell>
-              <TableCell className="">
-                <button
-                  className="flex items-center gap-1 text-left font-semibold"
-                  onClick={() => {
-                    open(item.path);
-                  }}>
-                  {item.type == "Folder" ?
-                    <Folder
-                      size={16}
-                      className="fill-yellow-400 text-yellow-400 dark:fill-yellow-500 dark:text-yellow-500"
-                    />
-                  : Icons({ name: item.name })}
-                  {item.name}
-                </button>
-              </TableCell>
-              <TableCell className="break-all">{item.path}</TableCell>
-              <TableCell>
-                <div className="flex flex-col items-center justify-center gap-1 sm:flex-row">
-                  <Button
-                    size="icon"
-                    className="h-8 w-8"
-                    variant="outline"
-                    disabled={index === 0}
-                    onClick={() => {
-                      [filteredFileList[index], filteredFileList[index - 1]] = [
-                        filteredFileList[index - 1],
-                        filteredFileList[index],
-                      ];
-                      const toSwap1 = fileList.findIndex(
-                        (find) => find.path === filteredFileList[index].path
-                      );
-                      const toSwap2 = fileList.findIndex(
-                        (find) => find.path === filteredFileList[index - 1].path
-                      );
-                      [fileList[toSwap1], fileList[toSwap2]] = [
-                        fileList[toSwap2],
-                        fileList[toSwap1],
-                      ];
-                      setFileList([...fileList]);
-                    }}>
-                    <ChevronUp />
-                  </Button>
-                  <Button
-                    size="icon"
-                    className="h-8 w-8"
-                    variant="outline"
-                    disabled={index === filteredFileList.length - 1}
-                    onClick={() => {
-                      [filteredFileList[index], filteredFileList[index + 1]] = [
-                        filteredFileList[index + 1],
-                        filteredFileList[index],
-                      ];
-                      const toSwap1 = fileList.findIndex(
-                        (find) => find.path === filteredFileList[index].path
-                      );
-                      const toSwap2 = fileList.findIndex(
-                        (find) => find.path === filteredFileList[index + 1].path
-                      );
-                      [fileList[toSwap1], fileList[toSwap2]] = [
-                        fileList[toSwap2],
-                        fileList[toSwap1],
-                      ];
-                      setFileList([...fileList]);
-                    }}>
-                    <ChevronDown />
-                  </Button>
-                  <Button
-                    size="icon"
-                    className="h-8 w-8"
-                    variant="destructive"
-                    onClick={() => {
-                      filteredFileList.splice(index, 1);
-                      const toDelete = fileList.findIndex((find) => find.path === item.path);
-                      fileList.splice(toDelete, 1);
-                      setFileList([...fileList]);
-                    }}>
-                    <Minus />
-                  </Button>
-                </div>
-              </TableCell>
-            </TableRow>
-          ))
+                    <Button
+                      size="icon"
+                      className="h-8 w-8"
+                      variant="outline"
+                      disabled={index === filteredFileList.length - 1}
+                      onClick={() => {
+                        [filteredFileList[index], filteredFileList[index + 1]] = [
+                          filteredFileList[index + 1],
+                          filteredFileList[index],
+                        ];
+                        const toSwap1 = fileList.findIndex(
+                          (find) => find.path === filteredFileList[index].path
+                        );
+                        const toSwap2 = fileList.findIndex(
+                          (find) => find.path === filteredFileList[index + 1].path
+                        );
+                        [fileList[toSwap1], fileList[toSwap2]] = [
+                          fileList[toSwap2],
+                          fileList[toSwap1],
+                        ];
+                        setFileList([...fileList]);
+                      }}>
+                      <ChevronDown />
+                    </Button>
+                    <Button
+                      size="icon"
+                      className="h-8 w-8"
+                      variant="destructive"
+                      onClick={() => {
+                        filteredFileList.splice(index, 1);
+                        const toDelete = fileList.findIndex((find) => find.path === item.path);
+                        fileList.splice(toDelete, 1);
+                        setFileList([...fileList]);
+                      }}>
+                      <Minus />
+                    </Button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            );
+          })
         : <TableRow>
             <TableCell colSpan={4} className="text-center">
               {fileList.length > 0 ? "No File or Folder Matched" : "No File or Folder Added"}
