2cc0c46245a57efacd1b9b955978c9d9ff9b87f3
Rafli Malik <Rafli.Malik@gmail.com>
2024-12-09 13:57:06 +0700

    chore: formatting

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,46 +15,46 @@ use tauri::async_runtime::Mutex as AsyncMutex;
 use terminal::*;
 
 fn main() {
-    let pty_system = native_pty_system();
+  let pty_system = native_pty_system();
 
-    let pty_pair = pty_system
-        .openpty(PtySize {
-            rows: 24,
-            cols: 80,
-            pixel_width: 0,
-            pixel_height: 0,
-        })
-        .unwrap();
+  let pty_pair = pty_system
+    .openpty(PtySize {
+      rows: 24,
+      cols: 80,
+      pixel_width: 0,
+      pixel_height: 0,
+    })
+    .unwrap();
 
-    let reader = pty_pair.master.try_clone_reader().unwrap();
-    let writer = pty_pair.master.take_writer().unwrap();
-    tauri::Builder::default()
-        .manage(AppState {
-            pty_pair: Arc::new(AsyncMutex::new(pty_pair)),
-            writer: Arc::new(AsyncMutex::new(writer)),
-            reader: Arc::new(AsyncMutex::new(BufReader::new(reader))),
-        })
-        .plugin(tauri_plugin_window_state::Builder::default().build())
-        .plugin(tauri_plugin_store::Builder::default().build())
-        .plugin(tauri_plugin_fs_extra::init())
-        .setup(|_app| {
-            db::init();
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![
-            get_all_remote_repo,
-            get_remote_repo_by_id,
-            insert_remote_repo,
-            edit_remote_repo,
-            edit_remote_repo_name,
-            edit_remote_repo_url,
-            delete_all_remote_repo,
-            delete_remote_repo_by_id,
-            async_write_to_pty,
-            async_resize_pty,
-            async_create_shell,
-            async_read_from_pty
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  let reader = pty_pair.master.try_clone_reader().unwrap();
+  let writer = pty_pair.master.take_writer().unwrap();
+  tauri::Builder::default()
+    .manage(AppState {
+      pty_pair: Arc::new(AsyncMutex::new(pty_pair)),
+      writer: Arc::new(AsyncMutex::new(writer)),
+      reader: Arc::new(AsyncMutex::new(BufReader::new(reader))),
+    })
+    .plugin(tauri_plugin_window_state::Builder::default().build())
+    .plugin(tauri_plugin_store::Builder::default().build())
+    .plugin(tauri_plugin_fs_extra::init())
+    .setup(|_app| {
+      db::init();
+      Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![
+      get_all_remote_repo,
+      get_remote_repo_by_id,
+      insert_remote_repo,
+      edit_remote_repo,
+      edit_remote_repo_name,
+      edit_remote_repo_url,
+      delete_all_remote_repo,
+      delete_remote_repo_by_id,
+      async_write_to_pty,
+      async_resize_pty,
+      async_create_shell,
+      async_read_from_pty
+    ])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
--- src-tauri/src/model/remote_repo.rs
+++ src-tauri/src/model/remote_repo.rs
@@ -7,8 +7,8 @@ use serde::Serialize;
 #[diesel(table_name = remote_repository)]
 
 pub struct RemoteRepo {
-    pub id: String,
-    pub repo_name: String,
-    pub repo_url: String,
-    pub created_at: NaiveDateTime,
+  pub id: String,
+  pub repo_name: String,
+  pub repo_url: String,
+  pub created_at: NaiveDateTime,
 }
